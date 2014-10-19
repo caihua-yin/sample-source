4954180b574ebf54f8a0129f9cd9d2cedd6b8466
alendyin <alend.yin@gmail.com>
2014-10-19 09:48:07 +0000

    Add shared library example, delete useless files

--- lib/static/main.c
+++ lib/static/main.c
@@ -1,10 +1,14 @@
 #include "algo.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
     int left = 8;
     int right = 5;
-    printf("%d add %d is %d\n", left, right, add(left, right));  
-    printf("%d sub %d is %d\n", left, right, sub(left, right));  
+    while (1) {
+        printf("%d add %d is %d\n", left, right, add(left, right));  
+        printf("%d sub %d is %d\n", left, right, sub(left, right));
+        sleep(1);
+    }
     return 0;
 }
